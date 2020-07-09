9f2bfc92d44ad7484e0ee483a17aa5a540d41843
rvignav <rvignav@gmail.com>
2020-07-08 18:58:54 -0700

    Fix JS instructions

--- src/components/ServiceDetails/InstallAndRunService/Javascript/index.js
+++ src/components/ServiceDetails/InstallAndRunService/Javascript/index.js
@@ -9,30 +9,26 @@ const Javascript = ({ classes }) => {
     <section className={classes.jsMainContainer}>
       <h2>Install</h2>
       <div>
-        <span>Install the SDK using npm</span>
+        <span>Install the SDK using npm.</span>
         <CodeSnippet>npm install snet-sdk</CodeSnippet>
       </div>
       <div>
         <span>
-          To generate the gRPC client libraries, you need the SingularityNET
-          Command Line Interface, or CLI, which you can install using pip
+          To generate the gRPC client libraries, you need the SingularityNET Command Line Interface, or CLI, which you
+          can install using pip.
         </span>
         <CodeSnippet>pip install snet-cli</CodeSnippet>
       </div>
       <div>
         <span>
-          Once you have the CLI installed, run the following command to generate
-          gRPC stubs for service
+          Once you have the CLI installed, run the following command to generate gRPC stubs for the Centivize service.
         </span>
-        <CodeSnippet>
-          snet sdk generate-client-library nodejs centivize-org centivize
-        </CodeSnippet>
-        <span>Run the code</span>
+        <CodeSnippet>snet sdk generate-client-library nodejs centivize-org centivize</CodeSnippet>
+        <span>Run the following code:</span>
         <CodeSnippet>
           import services from '&lt;path_to_grpc_service_file&gt;' <br />
           import messages from '&lt;path_to_grpc_message_file&gt;' <br />
-          const client = sdk.createServiceClient("centivize-org", "centivize",{" "}
-          "&lt;services.ClientStub&gt;") <br />
+          const client = sdk.createServiceClient("centivize-org", "centivize", "&lt;services.ClientStub&gt;") <br />
         </CodeSnippet>
       </div>
     </section>
